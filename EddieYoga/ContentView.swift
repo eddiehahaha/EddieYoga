c8a9b80e94468bd46b48fbd8a362d47518f6ea3f
eddiehahaha <30935571+eddiehahaha@users.noreply.github.com>
2020-09-24 11:01:21 -0500

    Initial Commit

---
+++ EddieYoga/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EddieYoga
+//
+//  Created by Man Kit Tsui on 9/24/20.
+//  Copyright © 2020 Man Kit Tsui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
